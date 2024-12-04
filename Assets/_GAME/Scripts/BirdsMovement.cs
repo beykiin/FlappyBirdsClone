e270873db4c56d1ed64d9ea6f9a4180f474843d7
Yasin Beken <yasinbeken16@gmail.com>
2024-12-04 18:38:30 +0300

    (Prefabs) Changed some prefabs settings in scene

--- Assets/_GAME/Scripts/BirdsMovement.cs
+++ Assets/_GAME/Scripts/BirdsMovement.cs
@@ -6,46 +6,47 @@ public class BirdsMovement : MonoBehaviour
     [SerializeField] private float maxTime = 0.5f;
     
 
-    private Rigidbody2D _rb;
+    //private Rigidbody2D _rb;
     private float _currentTime;
     private bool _isJumping;
     
 
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody2D>();
-        _isJumping = true;
+        //_rb = GetComponent<Rigidbody2D>();
+        //_isJumping = true;
     }
 
     private void Update()
     {
-        
+
 
         // Bastýkça sürekli artarak zýplama
-        Timer();
-        if (Input.GetKeyDown(KeyCode.Space) && _isJumping)
+        //Timer();
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
-            _currentTime = 0;
-            _isJumping = false;
+            //_currentTime = 0;
+            //_isJumping = false;
         }
     }
 
     void Jump()
     {
-        _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        //_rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        transform.position += Vector3.up * jumpForce * Time.deltaTime;
     }
 
-    void Timer()
-    {
-        if (!_isJumping)
-        {
-            _currentTime += Time.deltaTime;
-            if (_currentTime > maxTime)
-            {
-                _isJumping = true;
-            }
-        }
-        Debug.Log(_currentTime);
-    }
+    //void Timer()
+    //{
+    //    if (!_isJumping)
+    //    {
+    //        _currentTime += Time.deltaTime;
+    //        if (_currentTime > maxTime)
+    //        {
+    //            _isJumping = true;
+    //        }
+    //    }
+    //    Debug.Log(_currentTime);
+    //}
 }
